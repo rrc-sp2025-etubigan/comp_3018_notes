9f45f1ad9bde86a84f3347d87cf0fb9a3d839fac
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-02-24 16:39:42 -0600

    feat: define schema for item creation

--- src/api/v1/validation/itemSchema.ts
+++ src/api/v1/validation/itemSchema.ts
@@ -4,7 +4,36 @@ export const itemSchemas = {
     // POST createItem
     create: {
         body: Joi.object({
-
+            name: Joi.string()
+                .required()
+                .alphanum()
+                .max(24)
+                .messages({
+                    "any.required": "name is required",
+                    "string.empty": "name cannot be empty",
+                    "string.alphanum": "name must use alphanumeric \
+                                        characters only",
+                    "string.max": "name maximum length is 24 characters",
+                }),
+            quantity: Joi.number()
+                .required()
+                .integer()
+                .positive()
+                .messages({
+                    "any.required": "number is required",
+                    "number.empty": "number cannot be empty",
+                    "number.integer": "number must be an integer value",
+                    "number.positive": "number must be a positive value",
+                }),
+            category: Joi.string()
+                .required()
+                .valid("clothing", "tool", "food")
+                .messages({
+                    "any.required": "category is required",
+                    "string.empty": "category must not be empty",
+                    "any.valid": "category must these valid values \
+                                  [clothing, tool, food]",
+                }),
         }),
     },
 
