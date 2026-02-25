44c37598084d93923be1bcf9ff2b489ec2d6e204
Enrico Tubigan <solracden@gmail.com>
2026-02-24 22:50:07 -0600

    feat: complete function for creating document in firestore database

--- src/api/v1/repositories/firestoreRepository.ts
+++ src/api/v1/repositories/firestoreRepository.ts
@@ -1,16 +1,36 @@
 import { db } from "../../../config/firebaseConfig";
 
+const createId = async(): Promise<string> => {
+    try {
+        const itemInfoSnapshot = await db.collection("item-info").
+            doc("item-global-metrics").get();
+        const { ...data } = itemInfoSnapshot.data();
+        const padded_num = ((data["count"] as number) + 1).
+            toString().padStart(5, "0");
+        const id = `itemId_${padded_num}`;
+
+        return id;
+    } catch (error:unknown) {
+        const errorMessage =
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to create document id: ${errorMessage}`);
+    }
+};
+
 // POST createDocument
 export const createDocument = async<T>(
     collectionName: string,
     data: Partial<T>,
-): Promise<void> => {
+): Promise<string | null> => {
     try {
-        
+        const id = await createId();
+        await db.collection(collectionName).doc(id).set(data);
+
+        return id;
     } catch (error:unknown) {
         const errorMessage = 
             error instanceof Error ? error.message : "Unknown Error";
-        throw new Error(`Failed to [replace] document: ${errorMessage}`)
+        throw new Error(`Failed to [replace] document: ${errorMessage}`);
     }
 };
 
