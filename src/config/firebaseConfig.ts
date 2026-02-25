254811cb5c6f1512afa1012c62096124db952980
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-02-25 12:59:48 -0600

    feat: update firebase configuration to implement authentication

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,8 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getAuth, Auth } from "firebase-admin/auth";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-import * as serviceAccount from "./comp3018-personal-notes-firebase-adminsdk-fbsvc-8a0ee91b23.json";
+import serviceAccount from "./comp3018-personal-notes-firebase-adminsdk-fbsvc-8a0ee91b23.json";
 
 // Initialize the Firebase app with the service account credentials
 // This step is necessary before you can use any Firebase services
@@ -9,8 +10,10 @@ initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
 });
 
+const auth: Auth = getAuth();
+
 // Get a reference to the Firestore service
 // This creates a Firestore instance that you can use to interact with your database
 const db: Firestore = getFirestore();
 
-export { db };
+export { db, auth };
