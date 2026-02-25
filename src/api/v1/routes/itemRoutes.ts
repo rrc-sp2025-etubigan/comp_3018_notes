2e69d92b9e719d59f07e7100c348d0a4e66445c8
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-02-25 13:22:07 -0600

    feat: update item routes to include authentication and authorization in middleware stack

--- src/api/v1/routes/itemRoutes.ts
+++ src/api/v1/routes/itemRoutes.ts
@@ -2,13 +2,45 @@ import express from "express";
 import { validateRequest } from "../middleware/validation";
 import * as itemController from "../controllers/itemController";
 import { itemSchemas } from "../validation/itemSchema";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
 
 const itemRouter = express.Router();
 
-itemRouter.post("/", validateRequest(itemSchemas.create), itemController.createItemHandler);
-itemRouter.get("/", itemController.getAllItemsHandler);
-itemRouter.get("/:id", validateRequest(itemSchemas.getItemById), itemController.getItemByIdHandler);
-itemRouter.put("/:id", validateRequest(itemSchemas.update), itemController.updateItemHandler);
-itemRouter.delete("/:id", validateRequest(itemSchemas.delete), itemController.deleteItemHandler);
+itemRouter.post(
+    "/",
+    authenticate,
+    isAuthorized({ hasRole: [ "admin", "manager" ] }),
+    validateRequest(itemSchemas.create),
+    itemController.createItemHandler
+);
+
+itemRouter.get(
+    "/",
+    authenticate,
+    itemController.getAllItemsHandler);
+
+itemRouter.get(
+    "/:id",
+    authenticate,
+    validateRequest(itemSchemas.getItemById),
+    itemController.getItemByIdHandler
+);
+
+itemRouter.put(
+    "/:id",
+    authenticate,
+    isAuthorized({ hasRole: [ "admin", "manager" ], allowSameUser: true }),
+    validateRequest(itemSchemas.update),
+    itemController.updateItemHandler
+);
+
+itemRouter.delete(
+    "/:id",
+    authenticate,
+    isAuthorized({ hasRole: [ "admin", "manager" ] }),
+    validateRequest(itemSchemas.delete),
+    itemController.deleteItemHandler
+);
 
 export default itemRouter;
