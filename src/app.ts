2d3ed8bc2dea29d6097367fbfc4ede0f13ebfea3
Enrico Tubigan <solracden@gmail.com>
2026-02-25 01:20:02 -0600

    chore: update app.ts to use new item router

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import morgan from "morgan";
 
 // import the item routes from the new routes file
-//import placeholderRoute from "./api/v1/routes/placeholderRoute.ts";
+import itemRouter from "./api/v1/routes/itemRoutes";
 
 const app: Express = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(morgan("combined"));
 
 // Route handler for items
-//app.use("/api/v1", placeholderRoute);
+app.use("/api/v1/items", itemRouter);
 
 // Export the app
 export default app;
