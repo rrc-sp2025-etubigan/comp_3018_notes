edcb5842911499b142e136b970cdf793c7f871de
Enrico Tubigan <solracden@gmail.com>
2026-02-25 00:08:15 -0600

    feat: create item service file with template functions

---
+++ src/api/v1/services/itemService.ts
@@ -0,0 +1,76 @@
+import { Item } from "../models/itemModel";
+import * as firestoreRepository from "../repositories/firestoreRepository";
+
+const COLLECTION = "items"
+
+// POST createItem
+export const createItem = async(
+    data: {
+        name: string,
+        quantity: number,
+        category: string,
+    },
+): Promise<Item> => {
+    try {
+        
+    } catch (error: unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to create item: ${errorMessage}`);
+    }
+};
+
+// GET getItemById
+export const getItemById = async(
+    id: string,
+): Promise<Item> => {
+    try {
+        
+    } catch (error:unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to retrieve \
+                         item by id: ${errorMessage}`);
+    }
+};
+
+// GET getAllItems
+export const getAllItems = async(): Promise<Item[]> => {
+    try {
+        
+    } catch (error:unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to retrieve all \
+                         items: ${errorMessage}`);
+    }
+};
+
+// UPDATE updateItem
+export const updateItem = async(
+    id: string,
+    data: Pick<Item, "name" | "quantity" | "category">
+): Promise<Item> => {
+    try {
+        
+    } catch (error:unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to update \
+                         item: ${errorMessage}`);
+    }
+};
+
+// DELETE deleteItem
+export const deleteItem = async(
+    id: string,
+): Promise<void> => {
+    try {
+        
+    } catch (error: unknown) {
+        const errorMessage = 
+            error instanceof Error ? error.message : "Unknown Error";
+        throw new Error(`Failed to delete \
+                         item: ${errorMessage}`);
+    }
+};
