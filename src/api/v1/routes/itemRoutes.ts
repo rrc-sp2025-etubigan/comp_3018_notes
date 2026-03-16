e6a8e4aebc84edca4e98155df5aa7598ad125094
Enrico Tubigan <solracden@gmail.com>
2026-03-16 15:56:39 -0500

    docs: update inline documentation for POST request in itemRoutes

--- src/api/v1/routes/itemRoutes.ts
+++ src/api/v1/routes/itemRoutes.ts
@@ -7,6 +7,47 @@ import isAuthorized from "../middleware/authorize";
 
 const itemRouter = express.Router();
 
+/**
+ * @openapi
+ * /items:
+ *   post:
+ *     summary: Create new item
+ *     tags: [Items]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         schema:
+ *         type: object
+ *         required:
+ *           - name
+ *           - quantity
+ *           - category
+ *         properties:
+ *           name:
+ *             type: string
+ *             alphanum: true
+ *             maxLength: 24
+ *             example: "Hammer"
+ *           quantity:
+ *             type: integer
+ *             min: 1
+ *           category:
+ *             type: string
+ *             enum: [clothing, tool, food]
+ *     responses:
+ *       '201':
+ *         description: Item created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/validation/Items'
+ *       '400':
+ *         description: Invalid input data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/validations/Error'
+ */
 itemRouter.post(
     "/",
     authenticate,
